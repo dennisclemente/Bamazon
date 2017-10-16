aaf90f8510694d3f0703ccad7f1f9cc269e370fa
Dennis Clemente <dennisclemente@users.noreply.github.com>
2017-10-16 00:16:47 -0700

    Added strings, removed $ in pricing and ID nos

--- bamazon.sql
+++ bamazon.sql
@@ -1,44 +1,42 @@
-CREATE DATABASE Bamazon;
 
-USE bamazon;
-
+DROP TABLE if exists products;
 CREATE TABLE products (
-  itemId INT NOT NULL AUTO_INCREMENT,
-  productName VARCHAR(45) NULL,
-  departmentName DECIMAL(10,2) NULL,
-  price DECIMAL (5,2) NOT NULL,
-  stockQuantity INT NULL,
+  id INT NOT NULL AUTO_INCREMENT,
+  productName VARCHAR(45) not NULL,
+  departmentName VARCHAR(45) not NULL,
+  price INT (20) NOT NULL,
+  stockQuantity INT not NULL,
   PRIMARY KEY (id)
 );
 
-INSERT INTO products (itemId, productName, departmentName, price, stockQuantity)
-VALUES (00001, Leonardo_Da_Vinci, Books, $12.99, 500);
+INSERT INTO products (productName, departmentName, price, stockQuantity)
+VALUES ("Leonardo Da Vinci", "Books", 12.99, 500);
 
-INSERT INTO products (itemId, productName, departmentName, price, stockQuantity)
-VALUES (00002, Amazon Echo, Smart Home, $99.99, 300);
+INSERT INTO products (productName, departmentName, price, stockQuantity)
+VALUES ("Amazon Echo", "Smart Home", 99.99, 300);
 
-INSERT INTO products (itemId, productName, departmentName, price, stockQuantity)
-VALUES (00003, Macbook Pro, Computers, $1500.99, 200);
+INSERT INTO products (productName, departmentName, price, stockQuantity)
+VALUES ("Macbook Pro", "Computers", 1500.99, 200);
 
-INSERT INTO products (itemId, productName, departmentName, price, stockQuantity)
-VALUES (00004, Seventy-inch 4K HDTV, TV, $1300.99, 100);
+INSERT INTO products (productName, departmentName, price, stockQuantity)
+VALUES ("70-inch 4K HDTV", "TV", 1300.99, 100);
 
-INSERT INTO products (itemId, productName, departmentName, price, stockQuantity)
-VALUES (00005, Hitchcock Blu-Ray Collection, Movies, $200.99, 60);
+INSERT INTO products (productName, departmentName, price, stockQuantity)
+VALUES ("Hitchcock Blu-Ray Collection", "Movies", 200.99, 60);
 
-INSERT INTO products (itemId, productName, departmentName, price, stockQuantity)
-VALUES (00006, Queen Bedsheets, Home, $100.99, 1200);
+INSERT INTO products (productName, departmentName, price, stockQuantity)
+VALUES ("Queen Bedsheets", "Home", 100.99, 1200);
 
-INSERT INTO products (itemId, productName, departmentName, price, stockQuantity)
-VALUES (00007, Nike Shoes, Men Shoes, $180.99, 100);
+INSERT INTO products (productName, departmentName, price, stockQuantity)
+VALUES ("Nike Shoes", "Men Shoes", 180.99, 100);
 
-INSERT INTO products (itemId, productName, departmentName, price, stockQuantity)
-VALUES (00008, White Sneakers, Women Shoes, $70.99, 120);
+INSERT INTO products (productName, departmentName, price, stockQuantity)
+VALUES ("White Sneakers", "Women Shoes", 70.99, 120);
 
-INSERT INTO products (itemId, productName, departmentName, price, stockQuantity)
-VALUES (00009, Pet Leash, Pets, $20.99, 3000);
+INSERT INTO products (productName, departmentName, price, stockQuantity)
+VALUES ("Pet Leash", "Pets", 20.99, 3000);
 
-INSERT INTO products (itemId, productName, departmentName, price, stockQuantity)
-VALUES (000010, Microwave Oven, Appliances, $50.99, 400);
+INSERT INTO products (productName, departmentName, price, stockQuantity)
+VALUES ("Microwave Oven", "Appliances", 50.99, 400);
 
 
