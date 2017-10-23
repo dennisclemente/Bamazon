518c13ee5c1b22c83f98acda41f6bd2b1f9ea42f
Dennis Clemente <dennisclemente@users.noreply.github.com>
2017-10-23 12:21:38 -0700

    one question working

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,13 +1,12 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+// var Table = require("cli-table");
 
-var mysql = require('mysql');
-var inquirer = require('inquirer');
-
-//connection to mysql workbench
-  var connection = mysql.createConnection({
+var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
-  user: "", //username from mysql workbench
-  password: "", //password from mysql workbench
+  user: "root", //username from mysql workbench
+  password: "Hanapcoco8", //password from mysql workbench
   database: "bamazon"
   });
 
@@ -15,20 +14,24 @@ var inquirer = require('inquirer');
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
   
-  showCatalog();
+  pickItem();
   });
 
-  function showCatalog() {
+  function pickItem() {
+    // var items = [];
     connection.query('SELECT * FROM products', function(err, res) {
        if(err) throw err;
         for (var i = 0; i < res.length; i++) {
         console.log(res[i].id + ' | ' + res[i].productName + ' | ' + res[i].departmentName + ' | ' + res[i].price + ' | ' + res[i].stockQuantity); 
   }
         console.log("-----------------------------------"); 
-  });
+  });        connection.end();
+
 }
     //creates the function to be run when user is asked type of item and quantity to buy
-  inquirer.prompt([{
+  function pickItem() {
+      inquirer.prompt([
+    {
       type: 'input',
       message: "Enter ID for item you wish to buy",
       name: 'id'
@@ -37,28 +40,27 @@ var inquirer = require('inquirer');
       name: 'input',
       type: 'stockQuantity',
       message: "Interested? How many would you like to buy?"  
-  
-    }])
-    .then(function (order) {
-    var quantity = order.quantity;
-    var id = order.id;
-    connection.query("SELECT * FROM products = id", function(err, chosenItem) {
-    if (chosenItem[0].stockQuantity - quantity >= 0) {           
-        console.log("We have your item! I'll deliver it now!(chosenItem[0].productName");
-        // console.log(" Availability: " chosenItem[0].stockQuantity + " order: " quantity);
-        console.log("You will be charged "(order.quantity * chosenItem[0].price) +  " dollars. Thank you.");
-          connection.query('UPDATE products =' WHERE id=?, chosenItem[0].stockQuantity - quantity[0]),
-
-    function(showCatalog) {
-      if (err) throw err;
-      //    showCatalog();
-      // };  
+    }
+    ]).then(function(orderItem) {
+    var query = connection.query('SELECT * FROM products')
+      itemId: orderItem.id
+    }, function(err, res) {
+       console.log('Place your order now');
+       var quantity = res[0].stockQuantity - orderItem.stockQuantity;
+     if (orderItem.id, order.stockQuantity - quantity <= res[0].stockQuantity) {            
+        console.log("We have your item! I'll deliver it now!");
+        update(quantity, orderItem.id);
+        console.log("You will be charged: $" + orderItem.stockQuantity * res[0].price);
+     } else {
+        console.log("Insufficient quantity. However, we have ", order[0].stockQuantity)
+        update(quantity, orderItem.id);
     } 
-      else {
-      console.log("Insufficient quantity. However, we have " chosenItem[0].stockQuantity);
-      showCatalog();
+  });
+
+      function update(quantity, id) {
+      connection.query('UPDATE products WHERE id = ?, orderItem[0].stockQuantity - quantity[0]')
+      if (err) {
+        throw err;
+      }
     }
   };
-
-showCatalog();
-}
